c42847d8e004a5841245d7bca9e3182b5776a3e8
Usmanaholic <usmansinghi786@gmail.com>
2025-01-19 00:53:02 +0500

    security check

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -174,8 +174,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'usmansinghi786@gmail.com'  # Your Gmail address
-EMAIL_HOST_PASSWORD = 'dlpv fbsl ombj eypb'  # App password generated from step 2
+EMAIL_HOST_USER = ''  # Your Gmail address
+EMAIL_HOST_PASSWORD = ''  # App password generated from step 2
 
 
 
